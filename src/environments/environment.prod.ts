2f9a0e959c759d60634e367078007c0fa00cb69e
MarcusChrist <stressy.server@gmail.com>
2022-06-27 09:44:28 +0200

    gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,10 @@
 export const environment = {
   production: true,
-  api: 'https://nest-auth.ubbly.club/api',
-  socket: 'https://nest-auth.ubbly.club/',
+  api: 'https://riksmote.herokuapp.com/api',
+  socket: 'https://riksmote.herokuapp.com/',
   apps: {
     facebook: '535472397651204',
-    google:
-      '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
+    google: '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
     apple: {
       clientId: 'nest-auth.ubbly.club',
       redirectUri: 'https://nest-auth.ubbly.club/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,23 @@
 // export const environment = {
 //   production: false,
-//   api: 'http://localhost:3000',
-//   socket: 'http://localhost:3000',
+//   api: 'https://riksmote.herokuapp.com/api',
+//   socket: 'https://riksmote.herokuapp.com/',
 //   apps: {
-//     facebook: '513024756601831',
-//     google:
-//       '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
+//     facebook: '535472397651204',
+//     google: '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
 //     apple: {
-//       clientId: 'nest-auth.ubbly.club',
-//       redirectUri: 'https://nest-auth.ubbly.club/',
+//       clientId: '!nest-auth.ubbly.club',
+//       redirectUri: '!https://nest-auth.ubbly.club/',
 //     },
 //   },
 // };
 
 export const environment = {
-  production: true,
-  api: 'https://nest-auth.ubbly.club/api',
-  socket: 'https://nest-auth.ubbly.club/',
+  production: false,
+  api: 'http://localhost:3000',
+  socket: 'http://localhost:3000',
   apps: {
-    facebook: '535472397651204',
+    facebook: '513024756601831',
     google:
       '331672215174-0hlpm8fhjphiou05ovsd82vglor401ct.apps.googleusercontent.com',
     apple: {
